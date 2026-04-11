6dc62d6dfdaec18fdf8de89fd7c15e115698643b
[dyad] <crazyhacks123321@gmail.com>
2026-04-11 20:35:32 +0530

    [dyad] wrote 1 file(s)

--- src/pages/InfluencerIntelligence.tsx
+++ src/pages/InfluencerIntelligence.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState } from 'react';
 import DashboardLayout from '@/components/DashboardLayout';
 import InfluencerInput from '@/components/Influencer/InfluencerInput';
